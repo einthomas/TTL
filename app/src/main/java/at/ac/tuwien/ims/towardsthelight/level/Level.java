e4e23a2f4093d5351e4870035c3a82473af4be15
Thomas Koch <e1526232@student.tuwien.ac.at>
2016-11-02 13:59:35 +0100

    Implement new freeform-level-system

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/level/Level.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/level/Level.java
@@ -1,12 +1,12 @@
 package at.ac.tuwien.ims.towardsthelight.level;
 
 import android.content.Context;
-import android.util.Log;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Rect;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 
 /**
@@ -16,71 +16,74 @@ import java.util.ArrayList;
  */
 public class Level {
 
+    public static final byte BACKGROUND = 0;
+    public static final byte OBSTACLE = 1;
+    public static final byte COLLECTABLE = 2;
+
     public LevelInfo levelInfo;
     public ArrayList<Obstacle> obstacles;
     public int width, height;
+    public Rect rect, rectSource;
+    public Bitmap bitmap;
     private Context context;
+    private byte[] collisionData;
+
+    public float t, b;
 
     /**
      * Calls loadLevel to load the level according to the file name which is stored in levelInfo.
      *
      * @param levelInfo the level to be loaded.
      */
-    public Level(Context context, LevelInfo levelInfo) {
+    public Level(Context context, final int GAME_HEIGHT, final int GAME_WIDTH, LevelInfo levelInfo) {
         this.context = context;
         this.levelInfo = levelInfo;
+
         obstacles = new ArrayList<>();
-        if (!loadLevel()) {
-            Log.e(getClass().getName(), "Couldn't load level!");
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        bitmap = BitmapFactory.decodeResource(context.getResources(), levelInfo.imageResource, options);
+        rect = new Rect(0, 0, GAME_HEIGHT, GAME_WIDTH);
+        rectSource = new Rect(0, bitmap.getHeight() - 114, bitmap.getWidth(), bitmap.getHeight());
+
+        t = rectSource.top;
+        b = rectSource.bottom;
+
+
+        // process collision image
+        Bitmap collisionImage = BitmapFactory.decodeResource(context.getResources(), levelInfo.collisionResource, options);
+
+        collisionData = new byte[collisionImage.getWidth() * collisionImage.getHeight()];
+
+        IntBuffer pixelBuffer = IntBuffer.allocate(collisionImage.getWidth() * collisionImage.getHeight());
+        collisionImage.copyPixelsToBuffer(pixelBuffer);
+
+        for (int y = collisionImage.getHeight() - 1; y >= 0; y--) {
+            for (int x = 0; x < collisionImage.getWidth(); x++) {
+                int arrayPos = y * collisionImage.getWidth() + x;
+                int color = pixelBuffer.get(arrayPos);
+                arrayPos = collisionImage.getWidth() * (collisionImage.getHeight() - 1) - y * collisionImage.getWidth() + x;
+                if (Color.red(color) == 0 && Color.green(color) == 0 && Color.blue(color) == 0) {
+                    collisionData[arrayPos] = BACKGROUND;
+                } else if (Color.red(color) == 255 && Color.green(color) == 255 && Color.blue(color) == 255) {
+                    collisionData[arrayPos] = OBSTACLE;
+                } else {
+                    collisionData[arrayPos] = COLLECTABLE;
+                }
+            }
         }
     }
 
-    /**
-     * Reads the level file. The header of these files contain information about the width and height.
-     * A <tt>1</tt> represents an obstacle which is added to the <tt>obstacles</tt> list ordered by
-     * x and y coordinates.
-     *
-     * @return <tt>true</tt> if the level has been loaded successfully.
-     */
-    private boolean loadLevel() {
-        try {
-            InputStream inputStream = context.getAssets().open(levelInfo.fileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            try {
-                String line;
-
-                // read header
-                line = reader.readLine();
-                if (line != null) {
-                    String[] header = line.split(";");
-                    if (header.length == 2) {
-                        width = Integer.parseInt(header[0]);
-                        height = Integer.parseInt(header[1]);
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-
-                // read level
-                int y = 0;
-                while ((line = reader.readLine()) != null) {
-                    for (int x = 0; x < line.length(); x++) {
-                        if (line.charAt(x) == '1') {    // obstacle
-                            obstacles.add(new Obstacle(x, height - y));
-                        }
-                    }
-                    y++;
-                }
-            } finally {
-                reader.close();
-            }
-        } catch (IOException e) {
-            Log.e(getClass().getName(), "exception", e);
+    public boolean withinBounds(int x, int y) {
+        if (x < 0 || y < 0 || x >= bitmap.getWidth() || y >= bitmap.getHeight()) {
             return false;
         }
 
         return true;
+    }
+
+    public byte getCollisionData(int x, int y) {
+        return collisionData[y * bitmap.getWidth() + x];
     }
 }
