21d72c96f5c31fa739989863b606a084b8214082
Thomas Koch <e1526232@student.tuwien.ac.at>
2016-11-02 14:00:16 +0100

    Replace magic numbers by constants

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
@@ -2,9 +2,12 @@ package at.ac.tuwien.ims.towardsthelight;
 
 public class Player {
 
-    public static final float SLOWED_VELOCITY_Y = 1.0f;
-    public static final float MIN_VELOCITY_Y = 2.0f;
-    public static final float BOOST_Y = 4.0f;
+    public static final int SIZE_X = 4;
+    public static final int SIZE_Y = 4;
+    public static final float SPEED = 8.0f;
+    public static final float SLOWED_VELOCITY_Y = 8.0f;
+    public static final float MIN_VELOCITY_Y = 12.0f;
+    public static final float BOOST_SPEED = 4.0f;
     public float x, y;
     public float velocityX, velocityY;
 }
