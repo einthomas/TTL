e7de5ce2d4c61d8bff6557dad8774cfbf814f153
Felix Kugler <e1526144@student.tuwien.ac.at>
2016-11-13 18:54:49 +0100

    decrease base speed, increase acceleration

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
@@ -5,11 +5,11 @@ public class Player {
     public static final int SIZE_X = 4;
     public static final int SIZE_Y = 4;
     public static final float SPEED = 8.0f;
-    public static final float SLOWED_VELOCITY_Y = 8.0f;
-    public static final float BASE_VELOCITY_Y = 12.0f;
-    public static final float BASE_ACCELERATION = 16.0f;
-    public static final float BOOST_ACCELERATION = 4.0f;
-    public static final float DECELERATION = 16.0f;
+    public static final float SLOWED_VELOCITY_Y = 4.0f;
+    public static final float BASE_VELOCITY_Y = 8.0f;
+    public static final float BASE_ACCELERATION = 24.0f;
+    public static final float BOOST_ACCELERATION = 8.0f;
+    public static final float DECELERATION = 24.0f;
     public static final float INVINCIBILITY_TIME = 1.5f;
     public float x, y;
     public float velocityX, velocityY;
