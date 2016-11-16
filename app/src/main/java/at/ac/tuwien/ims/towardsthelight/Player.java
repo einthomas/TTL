d8f47de8e63c25c0036d3fb2f855ca6f4ba153f2
Felix Kugler <e1526144@student.tuwien.ac.at>
2016-11-16 21:12:42 +0100

    reduce base and boost acceleration

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/Player.java
@@ -7,9 +7,9 @@ public class Player {
     public static final float SPEED = 8.0f;
     public static final float SLOWED_VELOCITY_Y = 4.0f;
     public static final float BASE_VELOCITY_Y = 8.0f;
-    public static final float BASE_ACCELERATION = 24.0f;
-    public static final float BOOST_ACCELERATION = 8.0f;
-    public static final float DECELERATION = 24.0f;
+    public static final float BASE_ACCELERATION = 8.0f;
+    public static final float BOOST_ACCELERATION = 4.0f;
+    public static final float DECELERATION = 16.0f;
     public static final float INVINCIBILITY_TIME = 1.5f;
     public float x, y;
     public float velocityX, velocityY;
