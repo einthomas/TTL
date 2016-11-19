bf5b33ab252327edb7b97c3d6c878e6ea6e117e1
Thomas Koch <e1526232@student.tuwien.ac.at>
2016-11-19 18:50:01 +0100

    Write Collectable Javadoc

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/level/Collectable.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/level/Collectable.java
@@ -3,11 +3,30 @@ package at.ac.tuwien.ims.towardsthelight.level;
 import android.graphics.Bitmap;
 import android.graphics.RectF;
 
+/**
+ * Represents a collectable. Inherits from RectF.
+ *
+ * @author Thomas Koch
+ */
 public class Collectable extends RectF {
 
+    /**
+     * The loaded image to be drawn.
+     */
     public Bitmap bitmap;
+
+    /**
+     * <tt>true</tt> if is visible and therefore should be drawn.
+     */
     public boolean visible;
 
+    /**
+     * Creates a new collectable object. Sets {@link #visible} to false.
+     *
+     * @param left {@link RectF#left}
+     * @param top {@link RectF#top}
+     * @param bitmap {@link #bitmap}
+     */
     public Collectable(float left, float top, Bitmap bitmap) {
         super(left, top, left + bitmap.getWidth(), top - bitmap.getHeight());
         this.bitmap = bitmap;
