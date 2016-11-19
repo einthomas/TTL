43d663306d2cb013c8fec2971bb02f94b6bc1a3a
Felix Kugler <e1526144@student.tuwien.ac.at>
2016-11-19 19:40:36 +0100

    JavaDoc TTLSurfaceView

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/TTLSurfaceView.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/TTLSurfaceView.java
@@ -6,14 +6,35 @@ import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+/**
+ * Super class for all our SurfaceView implementation.
+ * This class handles scaling across all views.
+ * All drawing operations happen at aspect ratio dependent virtual resolution.
+ * @author Thomas Koch
+ * @author Felix Kugler
+ */
 public class TTLSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
-    // minimum width and height of virtual resolution
+    /**
+     * Minimum width of virtual resolution.
+     */
     protected final int GAME_WIDTH = 64;
+
+    /**
+     * Minimum height of virtual resolution.
+     */
     protected final int GAME_HEIGHT = 114; // ~9:16 aspect ratio
 
+    /**
+     * Transformations between virtual and real positions.
+     */
     protected Matrix gameMatrix, gameMatrixInverse;
 
+    /**
+     * Creates a new TTLSurfaceView
+     * @param context See <tt>SurfaceView</tt>.
+     * @param attrs See <tt>SurfaceView</tt>.
+     */
     public TTLSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -21,19 +42,33 @@ public class TTLSurfaceView extends SurfaceView implements SurfaceHolder.Callbac
         gameMatrixInverse = new Matrix();
     }
 
+    /**
+     * See <tt>SurfaceHolder.Callback</tt>.
+     */
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
     }
 
+    /**
+     * See <tt>SurfaceHolder.Callback</tt>.
+     */
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         calculateGameMatrix(width, height);
     }
 
+    /**
+     * See <tt>SurfaceHolder.Callback</tt>.
+     */
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
     }
 
+    /**
+     * Calculates {@link #gameMatrix} and {@link #gameMatrixInverse} for a given screen resolution.
+     * @param width Screen width.
+     * @param height Screen height.
+     */
     private void calculateGameMatrix(int width, int height) {
         // runs in UI thread
         gameMatrix.reset();
