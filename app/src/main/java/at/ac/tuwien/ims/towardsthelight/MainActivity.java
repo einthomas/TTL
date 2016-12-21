5863b95eb38931e2abcbc4d3143a48c57733c043
Thomas Koch <e1526232@student.tuwien.ac.at>
2016-12-21 14:38:40 +0100

    Set main menu fullscreen when resuming

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/MainActivity.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LOW_PROFILE |
             View.SYSTEM_UI_FLAG_FULLSCREEN |
@@ -28,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         );
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LOW_PROFILE |
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+        getSupportActionBar().hide();
     }
 }
