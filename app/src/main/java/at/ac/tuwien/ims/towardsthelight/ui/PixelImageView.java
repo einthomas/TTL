d551f98b2e1cc20f88ada083152ca8bcb7409386
Felix Kugler <e1526144@student.tuwien.ac.at>
2016-12-26 16:37:18 +0100

    create PixelImageView to draw pixelized images in XML

---
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/ui/PixelImageView.java
@@ -0,0 +1,103 @@
+package at.ac.tuwien.ims.towardsthelight.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import at.ac.tuwien.ims.towardsthelight.R;
+
+/**
+ * Created by Felix on 26.12.2016.
+ */
+
+public class PixelImageView extends View {
+
+    private Bitmap image;
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    /**
+     * Simple constructor to use when creating a view from code.
+     *
+     * @param context The Context the view is running in, through which it can
+     *                access the current theme, resources, etc.
+     */
+    public PixelImageView(Context context) {
+        super(context);
+    }
+
+    /**
+     * Constructor that is called when inflating a view from XML. This is called
+     * when a view is being constructed from an XML file, supplying attributes
+     * that were specified in the XML file. This version uses a default style of
+     * 0, so the only attribute values applied are those in the Context's Theme
+     * and the given AttributeSet.
+     * <p>
+     * <p>
+     * The method onFinishInflate() will be called after all children have been
+     * added.
+     *
+     * @param context The Context the view is running in, through which it can
+     *                access the current theme, resources, etc.
+     * @param attrs   The attributes of the XML tag that is inflating the view.
+     * @see View(Context, AttributeSet, int)
+     */
+    public PixelImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /**
+     * Perform inflation from XML and apply a class-specific base style from a
+     * theme attribute. This constructor of View allows subclasses to use their
+     * own base style when they are inflating. For example, a Button class's
+     * constructor would call this version of the super class constructor and
+     * supply <code>R.attr.buttonStyle</code> for <var>defStyleAttr</var>; this
+     * allows the theme's button style to modify all of the base view attributes
+     * (in particular its background) as well as the Button class's attributes.
+     *
+     * @param context      The Context the view is running in, through which it can
+     *                     access the current theme, resources, etc.
+     * @param attrs        The attributes of the XML tag that is inflating the view.
+     * @param defStyleAttr An attribute in the current theme that contains a
+     *                     reference to a style resource that supplies default values for
+     *                     the view. Can be 0 to not look for defaults.
+     * @see View(Context, AttributeSet)
+     */
+    public PixelImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.PixelImageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        int imageId = attributes.getResourceId(R.styleable.PixelImageView_imageResource, 0);
+        setImage(BitmapFactory.decodeResource(context.getResources(), imageId, options));
+    }
+
+    /**
+     * Implement this to do your drawing.
+     *
+     * @param canvas the canvas on which the background will be drawn
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawColor(0xff000000);
+
+        if (image != null) {
+            canvas.drawBitmap(image, 0, 0, null);
+        }
+    }
+}
