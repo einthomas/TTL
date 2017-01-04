b984f9f239ad608de7aa987632bb644b1c9f2ccd
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-01-04 21:47:15 +0100

    Make game over activity fullscreen

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/GameOverActivity.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/GameOverActivity.java
@@ -10,6 +10,13 @@ public class GameOverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_game_over);
     }
 
@@ -22,6 +29,18 @@ public class GameOverActivity extends AppCompatActivity {
         playerAnimation.start();
 
         super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getSupportActionBar().hide();
     }
 
     public void buttonBackClicked(View view) {
