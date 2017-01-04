15563b6aa23c897411d8449c84c28084cd558163
Felix Kugler <e1526144@student.tuwien.ac.at>
2017-01-04 21:53:40 +0100

    add view animations for game over screen

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/GameOverActivity.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/GameOverActivity.java
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -15,13 +16,10 @@ public class GameOverActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
-        ObjectAnimator playerAnimation = ObjectAnimator.ofFloat(
-            findViewById(R.id.player), "y", -7, 114 - 7
-        );
-        playerAnimation.setDuration(1500);
-        playerAnimation.start();
-
         super.onStart();
+
+        findViewById(R.id.player).startAnimation(AnimationUtils.loadAnimation(this, R.anim.player_falling));
+        findViewById(R.id.game_over_title).startAnimation(AnimationUtils.loadAnimation(this, R.anim.text_in));
     }
 
     public void buttonBackClicked(View view) {
