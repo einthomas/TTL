e22649e4e7d03466421fcf507c4d2dfb25662653
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-01-05 13:58:31 +0100

    Write PauseMenuActivity javadoc

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/PauseMenuActivity.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/PauseMenuActivity.java
@@ -1,16 +1,28 @@
 package at.ac.tuwien.ims.towardsthelight;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import at.ac.tuwien.ims.towardsthelight.ui.MenuButtonXML;
 
+/**
+ * The pause menu. Contains an option to mute the music and display the help activity.
+ * <tt>activity_pause</tt> is the corresponding layout.
+ * @author Felix Kugler
+ * @author Thomas Koch
+ */
 public class PauseMenuActivity extends AppCompatActivity {
+
+    /**
+     * The mute music button.
+     */
     private MenuButtonXML muteButton;
 
+    /**
+     * Sets the activity to fullscreen and sets the content view.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +41,9 @@ public class PauseMenuActivity extends AppCompatActivity {
         muteButton.text = GameSurfaceView.muted ? getString(R.string.unmute) : getString(R.string.mute);
     }
 
+    /**
+     * Makes sure the activity is fullscreen (againt).
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -41,14 +56,29 @@ public class PauseMenuActivity extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
+    /**
+     * "Closes" the activity by calling {@link #finish()}.
+     *
+     * @param view The current view.
+     */
     public void buttonResumeClicked(View view) {
         finish();
     }
 
+    /**
+     * Starts the activity {@link HelpActivity}.
+     *
+     * @param view The current view.
+     */
     public void buttonHelpClicked(View view) {
         startActivity(new Intent(this, HelpActivity.class));
     }
 
+    /**
+     * Toggles {@link GameSurfaceView#muted} and redraws the button by calling invalidate().
+     *
+     * @param view The current view.
+     */
     public void buttonMusicToggle(View view) {
         GameSurfaceView.muted = !GameSurfaceView.muted;
         muteButton.text = GameSurfaceView.muted ? getString(R.string.unmute) : getString(R.string.mute);
