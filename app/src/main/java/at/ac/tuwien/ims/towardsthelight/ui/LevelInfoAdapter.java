1b4c3ce8918b9a88782794f06ba5b02463e0dcb0
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-01-05 13:13:08 +0100

    Write LevelInfoAdapter javadoc

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/ui/LevelInfoAdapter.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/ui/LevelInfoAdapter.java
@@ -17,15 +17,39 @@ import java.util.Locale;
 import at.ac.tuwien.ims.towardsthelight.R;
 import at.ac.tuwien.ims.towardsthelight.level.LevelInfo;
 
+/**
+ * The bridge between the level data and the ListView contained in <tt>activity_level_selection.xml</tt>.
+ *
+ * @author Thomas Koch
+ */
 public class LevelInfoAdapter extends ArrayAdapter<LevelInfo> {
 
+    /**
+     * The current context.
+     */
     private final Context context;
 
+    /**
+     * @param context The current context.
+     * @param resource The resource ID for a layout file.
+     * @param levelInfos The {@link LevelInfo} objects.
+     */
     public LevelInfoAdapter(Context context, int resource, List<LevelInfo> levelInfos) {
         super(context, resource, levelInfos);
         this.context = context;
     }
 
+    /**
+     * For each level a border, the achieved medals, the number of the level, the best time and the
+     * highscore is drawn. A diamond border is drawn if the highscore is greater than the largest
+     * score in the resource file <tt>levelx_medal_points</tt>, where <tt>x</tt> is
+     * <tt>{@link LevelInfo#number} - 1</tt>. The resource file also decides which medals are displayed.
+     *
+     * @param position The position of the item within the LevelInfo collection.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent The ListView contained in activity_level_selection.xml.
+     * @return a view that represents a {@link LevelInfo} object.
+     */
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -89,6 +113,13 @@ public class LevelInfoAdapter extends ArrayAdapter<LevelInfo> {
         return convertView;
     }
 
+    /**
+     * Returns the {@link LevelInfo} at position <tt>getCount() - position - 1</tt> so that the
+     * level are ordered descending by their number.
+     *
+     * @param position The position of the {@link LevelInfo} object that's accessed.
+     * @return The item at position getCount() - position - 1.
+     */
     @Override
     public LevelInfo getItem(int position) {
         return super.getItem(super.getCount() - position - 1);
