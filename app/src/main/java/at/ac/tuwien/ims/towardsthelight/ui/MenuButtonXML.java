34c4521b2234703e6f04587f7dd5b58cb04f119d
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-01-05 13:16:42 +0100

    Write MenuButtonXML javadoc

--- app/src/main/java/at/ac/tuwien/ims/towardsthelight/ui/MenuButtonXML.java
+++ app/src/main/java/at/ac/tuwien/ims/towardsthelight/ui/MenuButtonXML.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.ims.towardsthelight.ui;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -9,8 +10,23 @@ import android.widget.RelativeLayout;
 
 import at.ac.tuwien.ims.towardsthelight.R;
 
+/**
+ * A menu button intended to be used in XML. Uses the bitmaps for {@link #releasedBitmap} and
+ * {@link #pressedBitmap} associated with R.drawable.menu_button and R.drawable.menu_button_pressed.
+ *
+ * @author Thomas Koch
+ */
 public class MenuButtonXML extends ImageButtonXML {
 
+    /**
+     * Sets {@link #releasedBitmap} to the bitmap associated with R.drawable.menu_button and
+     * {@link #releasedBitmap} to the bitmap associated with R.drawable.menu_button_pressed.
+     * Also sets the {@link #font} to {@link SpriteFont#mainFont(Resources)} and the {@link #position}
+     * according to the size of the bitmaps.
+     *
+     * @param context The current context.
+     * @param attributeSet Th attribute set.
+     */
     public MenuButtonXML(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
